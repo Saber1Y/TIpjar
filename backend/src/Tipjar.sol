0d99de08c3d332584fd5c704faf5bb27968a0fe4
ayodeji <ayodejiakintobi1@gmail.com>
2024-12-07 21:47:32 +0100

    UPDATE: OPENZEPPLIN ADDED

--- backend/src/Tipjar.sol
+++ backend/src/Tipjar.sol
@@ -1,16 +1,22 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.17;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/IERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract TipJar {
-    address public owner;
+    address public owner; //iniialise owner variable
 
-    error NoRecipientAddress();
-    error NoFundsToWithdraw();
+    error NoRecipientAddress(); //fall back error message
+    error NoFundsToWithdraw(); //fall back error message
+    error NotContractOwner()
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Not the contract owner");
+        if (msg.sender != owner) {
+            revert NotContractOwner(); //revert error message
+        }
         _;
-    }
+    } //condiition to check owner variable
 
     constructor() {
         owner = msg.sender; // Set the contract deployer as the owner
@@ -23,7 +29,7 @@ contract TipJar {
 
         (bool success, ) = payable(_recipient).call{value: msg.value}("");
         require(success, "Transaction failed");
-    }
+    } 
 
     function withdraw() external onlyOwner {
         uint256 balance = address(this).balance;
