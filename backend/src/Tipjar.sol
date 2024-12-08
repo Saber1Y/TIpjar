d2ebd18ae9a60e2e298556a308b6758aaa3efc3c
ayodeji <ayodejiakintobi1@gmail.com>
2024-12-08 23:18:39 +0100

    UPDATE: SOL BACKEND UPDATED

--- backend/src/Tipjar.sol
+++ backend/src/Tipjar.sol
@@ -1,43 +1,50 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+//SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
+
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/access/Ownable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/IERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract TipJar {
-    address public owner; //iniialise owner variable
+contract TipJar is Ownable {
+    using SafeERC20 for IERC20;
 
-    error NoRecipientAddress(); //fall back error message
-    error NoFundsToWithdraw(); //fall back error message
-    error NotContractOwner()
+    event Payment(address indexed sender, uint256 amount);
+    event Withdraw(address indexed recipient, uint256 amount);
+    event WithdrawERC20(
+        address indexed recipient,
+        address indexed token,
+        uint256 amount
+    );
 
-    modifier onlyOwner() {
-        if (msg.sender != owner) {
-            revert NotContractOwner(); //revert error message
-        }
-        _;
-    } //condiition to check owner variable
-
-    constructor() {
-        owner = msg.sender; // Set the contract deployer as the owner
+    receive() external payable {
+        emit Payment(msg.sender, msg.value);
     }
 
-    function sendTip(address _recipient) external payable {
-        if (_recipient == address(0)) {
-            revert NoRecipientAddress();
-        }
-
-        (bool success, ) = payable(_recipient).call{value: msg.value}("");
-        require(success, "Transaction failed");
-    } 
+    function deposit() external payable {
+        emit Payment(msg.sender, msg.value);
+    }
 
     function withdraw() external onlyOwner {
-        uint256 balance = address(this).balance;
-        if (balance == 0) {
-            revert NoFundsToWithdraw();
-        }
+        uint256 amount = address(this).balance;
 
-        (bool sent, ) = payable(msg.sender).call{value: balance}("");
+        (bool sent, ) = (msg.sender).call{value: amount}("");
         require(sent, "Failed to send Ether");
+
+        emit Withdraw(msg.sender, amount);
+    }
+
+    function transferEth(address _to, uint256 _amount) external onlyOwner {
+        (bool sent, ) = _to.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+        emit Withdraw(_to, _amount);
+    }
+
+    function transferERC20(
+        address _token,
+        address _to,
+        uint256 _amount
+    ) external onlyOwner {
+        IERC20(_token).safeTransfer(_to, _amount);
+        emit WithdrawERC20(_to, _token, _amount);
     }
 }
