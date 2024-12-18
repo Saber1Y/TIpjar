2f09305898ce3d4b4bb947a85b4c52be5b81236f
ayodeji <ayodejiakintobi1@gmail.com>
2024-12-18 22:16:39 +0100

    UPDATE: OPENZEPPLIN

--- backend/src/Tipjar.sol
+++ backend/src/Tipjar.sol
@@ -1,9 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/access/Ownable.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/IERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
+import "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract TipJar is Ownable {
     using SafeERC20 for IERC20;
