e3be7cc7f576991fe7a1621f9e78797795a23573
ayodeji <ayodejiakintobi1@gmail.com>
2025-04-27 21:23:49 +0100

    UPDATE: FACTORY CONTRACT ADDED

--- backend/src/TIpjarFactory.sol
+++ backend/src/TIpjarFactory.sol
@@ -1,3 +1,22 @@
-//SPDX-license-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
+
+import "./Tipjar.sol";
+
+contract TipJarFactory {
+    event TipJarCreated(address indexed owner, address tipJar);
+
+    address[] public allTipJars;
+
+    function createTipJar() external returns (address) {
+        TipJar tipJar = new TipJar(msg.sender);
+        allTipJars.push(address(tipJar));
+
+        emit TipJarCreated(msg.sender, address(tipJar));
+        return address(tipJar);
+    }
+
+    function getAllTipJars() external view returns (address[] memory) {
+        return allTipJars;
+    }
+}
