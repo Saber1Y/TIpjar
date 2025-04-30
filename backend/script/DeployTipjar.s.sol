2dc017b1ab06022807ecf7f1d815da12c580a145
ayodeji <ayodejiakintobi1@gmail.com>
2025-04-30 14:53:02 +0100

    UPDATE: DEPLOYSS

---
+++ backend/script/DeployTipjar.s.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+import {TipJar} from "../src/Tipjar.sol";
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
+
+contract DeployTipjar is Script {
+    function run() external  {
+        vm.startBroadcast();
+
+        TipJar tipJar = new TipJar(msg.sender);
+
+        vm.stopBroadcast();
+
+        console.log("TipJar deployed at:", address(tipJar));
+
+    }
+}
